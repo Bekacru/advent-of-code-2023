456ca2b471cb2609bd7fea0e18cd9185a79da237
bekacru <bekacru@gmail.com>
2023-12-02 00:38:46 +0300

    feat: day 1

---
+++ day-1/1.swift
@@ -0,0 +1,46 @@
+import Foundation
+
+let input = try! String(contentsOf: URL(filePath: "./input-1.txt"), encoding: .utf8)
+
+func calibrator(input: String) -> String {
+  var result = 0
+  let lines = input.split(separator: "\n")
+  let numbers = lines.map {
+    line in
+    var firstNum: Int?
+    var lastNum: Int?
+    line.forEach {
+      char in
+      if char.isNumber {
+        if let num = firstNum {
+          lastNum = Int(String(char))
+        } else {
+          firstNum = Int(String(char))
+        }
+      }
+    }
+    if let lastNum {
+      return firstNum! * 10 + lastNum
+    } else {
+      return firstNum! * 10 + firstNum!
+    }
+  }
+
+  return String(numbers.reduce(0, +))
+}
+
+let strMap = [
+  "one": "o1e",
+  "two": "t2o",
+  "three": "t3e",
+  "four": "f4r",
+  "five": "f5e",
+  "six": "s6x",
+  "seven": "s7n",
+  "eight": "e8t",
+  "nine": "n9e",
+]
+var newInput = input
+strMap.forEach { newInput = newInput.replacingOccurrences(of: $0.key, with: $0.value) }
+
+print(calibrator(input: input), calibrator(input: newInput))
